f568323628260e9521ef77ceb1417de8bc86dd45
alicegin <ordazr.ximena@gmail.com>
2024-08-15 13:06:06 -0600

    header y función para que aparezca el título de forma dinámica

--- functions.php
+++ functions.php
@@ -1,4 +1,10 @@
 <?php
+//Añadir el título de forma dinámica
+function deportesUAQ_theme_support() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme','deportesUAQ_theme_support');
 //Función para insertar el css de manera dinámica (hay que utilizar el código "wp_head();" en la página que queremos poner los stylesheets)
 function deportesUAQ_register_styles(){
     wp_enqueue_style('deportesUAQ-style',get_template_directory_uri() . "/assets/css/style.css", array(), '1.0');
