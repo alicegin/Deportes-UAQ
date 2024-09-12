5a2c35ced52986c3c881ff77d48b00fc62cddd3b
alicegin <ordazr.ximena@gmail.com>
2024-09-12 09:51:33 -0600

    Cambio de la página de index
    Se tiene que cambiar la página en Wordpress de la página front page a la de inicio, y cambiar el template de la página de especialización deportiva a el template con ese nombre.

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
-<?php get_template_part('template-parts/header'); ?>
-<main>
-    <!-- Contenido aquí -->
-    <?php
-    include 'template-parts/slider.php';
-
-    $imagenes = [
-      'http://deportesuaq.mx/wp-content/uploads/2024/07/1A-2-scaled.jpg',
-      'http://deportesuaq.mx/wp-content/uploads/2024/07/3A-2.jpg',
-      'http://deportesuaq.mx/wp-content/uploads/2024/07/4A-2-scaled.jpg',
-      'http://deportesuaq.mx/wp-content/uploads/2024/07/5A-1.jpg',
-      'http://deportesuaq.mx/wp-content/uploads/2024/07/6A-1.jpg'
-  ];
-
-
-  echo generarCarrusel($imagenes);
-    
-    ?>
-    <section class="margen">
-      <div class="contenedorCentrar" id="socialMedia">
-      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fuaqdeportes&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1835712586922845" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-        <iframe src="https://widget.taggbox.com/161494" style="border:none;" width="340" height="500"></iframe>
-      </div>
-    </section>
-</main>
-<?php get_template_part('template-parts/footer'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Deportes UAQ</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
+    <nav>
+        <a href="">¿Quiénes somos?</a>
+        <a href="<?php echo home_url('/EspecializacionDep'); ?>">Especialización deportiva</a>
+        <a href="">Deportes para todos</a>
+        <a href="">Salón de la fama</a>
+        <a href="">Instalaciones</a>
+        <a href="">E-Sports</a>
+    </nav>
+</body>
+</html>
