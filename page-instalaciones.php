afcb02ea556495bcc7673d49205ce006dc8af0d4
PedroBen <pedroben83@outlook.com>
2024-11-23 14:22:03 -0600

    cambios de las instalaciones

--- page-instalaciones.php
+++ page-instalaciones.php
@@ -40,6 +40,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                             </svg>
+                            <label style="display: none;">Ubicación</label>
                             <p class="direccion">Carretera a Celaya Km. 3, Corregidora 5, Emiliano Zapata, 76912 El Pueblito, Qro.</p>
                         </div>
                     </div>
@@ -113,6 +114,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                             </svg>
+                            <label style="display: none;">Ubicación</label>
                             <p class="direccion">Cerro de las Campanas, s/n, Col. Las Campanas, Qro.</p>
                         </div>
                     </div>
@@ -186,6 +188,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                             </svg>
+                            <label style="display: none;">Ubicación</label>
                             <p class="direccion">C. Ignacio Manuel Altamirano Sur 24, La Santa Cruz, 36, Centro, Qro.</p>
                         </div>
                     </div>
@@ -259,6 +262,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                             </svg>
+                            <label style="display: none;">Ubicación</label>
                             <p class="direccion">Carretera a Chichimequillas s/n, Ejido Bolaños, 76140 Santiago de Querétaro, Qro.</p>
                         </div>
                     </div>
