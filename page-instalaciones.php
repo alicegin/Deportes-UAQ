e4723d05e7c53882bd55bfc3ebe7b6bd371a61ff
PedroBen <84807249+ByOswaldo65@users.noreply.github.com>
2025-01-14 11:45:21 -0600

    update gatienda e instalaciones

--- page-instalaciones.php
+++ page-instalaciones.php
@@ -13,80 +13,6 @@
         <section class="cardContainer">
             <div class="card">
                 <p class="precio">Contáctanos</p>
-                <h1 class="nombre">Alberca El Pueblito</h1>
-                <div class="hiddenImgContainer">
-                    <div class="hiddenImg">
-                        <img src="http://deportesuaq.mx/wp-content/uploads/2024/11/alberca_1.jpg" alt="Alberca El Pueblito UAQ" />
-                    </div>
-                    <div class="hiddenImg">
-                        <img src="http://deportesuaq.mx/wp-content/uploads/2024/11/alberca_2.jpg" alt="Alberca El Pueblito UAQ" />
-                    </div>
-                    <div class="hiddenImg">
-                        <img src="http://deportesuaq.mx/wp-content/uploads/2024/11/alberca_4.jpg" alt="Alberca El Pueblito UAQ" />
-                    </div>
-                </div>
-                <div class="hiddenContainer">
-                    <div class="moreInfo">
-                        <div class="parteMoreInfo">
-                            <label>Descripción</label>
-                            <p class="desc">Clases para niños y adultos. Se cuenta con distintos horarios</p>
-                        </div>
-                        <div class="parteMoreInfo">
-                            <label>Características</label>
-                            <p class="caracteristicas">10 carriles en curso largo y 20 en curso corto</p>
-                        </div>
-                        <div class="parteIcon infoDireccion">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                            </svg>
-                            <label style="display: none;">Ubicación</label>
-                            <p class="direccion">Carretera a Celaya Km. 3, Corregidora 5, Emiliano Zapata, 76912 El Pueblito, Qro.</p>
-                        </div>
-                    </div>
-                    <div class="contactoContainer">       
-                        <label>Contacto</label>                           
-                        <div class="parteIcon">
-                            <p class="correo">correo@gmail.com</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                            </svg>                            
-                        </div>
-                        <div class="parteIcon">
-                            <p class="tel">555-555-5555</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
-                            </svg>                          
-                        </div>
-                    </div>
-                </div>
-                <div class="imgContainer">
-                    <img src="http://deportesuaq.mx/wp-content/uploads/2024/11/alberca_3.jpg" alt="Instalaciones UAQ" />
-                </div>
-                <div class="infoContainer">                    
-                    <div class="detallesContainer">
-                        <div class="cardDetalle">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                            </svg>
-                            <p>100</p>
-                        </div>
-                        <div class="cardDetalle">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
-                            </svg>
-                            <p>50 m x 25 m x 2.7 m</p>
-                        </div>
-                        <div class="cardDetalle btnMas">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                            </svg>                                                      
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <p class="precio">Contáctanos</p>
                 <h1 class="nombre">Estadio Centro Universitario</h1>
                 <div class="hiddenImgContainer">
                     <div class="hiddenImg">
@@ -308,8 +234,7 @@
                 </div>
             </div>
         </section>
-    </main>    
-    <?php get_template_part('template-parts/footer'); ?>
+    </main>
 
     <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.7.1.min.js"></script>
     <script>
